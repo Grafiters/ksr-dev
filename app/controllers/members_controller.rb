fe5b960cd4a6f7a0f7bc6357c47569c6049ec189
Grafiters <bayugrafit@gmail.com>
2019-11-16 20:30:23 +0700

    create make create

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,21 @@
 class MembersController < ApplicationController
+    def index
+        
+    end
+    def new
+        @member = Member.new
+    end
+    def create
+        @member = Member.new(params_a)
+        if @member.save
+            redirect_to members_path
+        else
+            redirect_to new_members_path
+        end
+    end
+    private
+        def params_a
+            params.require(:member).permit(:name, :nim, :fakultas)
+        end
+        
 end
