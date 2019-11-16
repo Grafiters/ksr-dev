8999b6fb6c85c8aa074b65d6ca515592de5e73b3
Grafiters <bayugrafit@gmail.com>
2019-11-16 20:16:34 +0700

    new controller & model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_16_131532) do
+
+  create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "nim"
+    t.string "fakultas"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
