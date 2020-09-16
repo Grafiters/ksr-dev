db9cdf93f8ba5cd3f5511a4acbca9ee0bafbb934
Grafiters <bayugrafit@gmail.com>
2020-09-17 03:17:33 +0700

    update simple dashboard layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  # get 'dasboard/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :members
   resources :events
 
   get '/', to: 'homes#index'
+  get '/dashboard', to: 'dasboard#index'
+  get '/dashboard/member', to: 'members#index'
+  get '/dashboard/event', to: 'events#index'
 
 end
